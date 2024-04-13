ebae96334df3e7f55cbf79470b6d5b76bd1b2df1
Valente2 <valentevescio@gmail.com>
2024-04-12 21:38:21 -0400

    added terminal feature

--- LLPterm.py
+++ LLPterm.py
@@ -1,5 +1,15 @@
+import os
 print("lovelace linux terminal\n[c]2024 GPL 3.0 Licence")
-name=input("")
+name=input("enter name ")
+host=input("enter hostname ")
 run=True
 while run==True:
-    cmd=input("$")
+    print(name,"@",host," LLPT")
+    cmd=input("$ ")
+    if cmd=="bash" or  "Batch":
+        run2=True
+        while run2==True:
+            cmd=input("# ")
+            os.system(cmd)
+    if cmd=="package":
+        pass
